b86d3acf9fda5bde8be0e716dcc353d587458317
hikarumatsunaka <mattu.h1103@gmail.com>
2022-09-19 23:11:50 +0900

    updated.

--- src/index.tsx
+++ src/index.tsx
@@ -16,20 +16,20 @@ ReactDOM.render(
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCgNoADoug7k1fF_GGRwBU0tFyXJuPiYcU",
-  authDomain: "react-ts-netflix-clone.firebaseapp.com",
-  projectId: "react-ts-netflix-clone",
-  storageBucket: "react-ts-netflix-clone.appspot.com",
-  messagingSenderId: "46073989116",
-  appId: "1:46073989116:web:1e061e066aa8d0b4335479",
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCgNoADoug7k1fF_GGRwBU0tFyXJuPiYcU",
+//   authDomain: "react-ts-netflix-clone.firebaseapp.com",
+//   projectId: "react-ts-netflix-clone",
+//   storageBucket: "react-ts-netflix-clone.appspot.com",
+//   messagingSenderId: "46073989116",
+//   appId: "1:46073989116:web:1e061e066aa8d0b4335479",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
